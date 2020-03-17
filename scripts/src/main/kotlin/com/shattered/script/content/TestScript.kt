63c06f3fd2d0a02a41b2c5391d28881e4a09e3c2
GrizzlyEntertainment <47308540+GrizzlyEntertainment@users.noreply.github.com>
2020-03-17 00:56:41 -0500

    Added a test script for on awake.

---
+++ scripts/src/main/kotlin/com/shattered/script/content/TestScript.kt
@@ -0,0 +1,12 @@
+package com.shattered.script.content
+
+import com.shattered.script.api.impl.character.CharacterAPI
+import com.shattered.script.types.CharacterScript
+
+class TestScript : CharacterScript() {
+
+
+    override fun onAwake(api: CharacterAPI?) {
+        println(api?.channel?.sendDefaultMessage("This is from the MS Surface Pro."))
+    }
+}
