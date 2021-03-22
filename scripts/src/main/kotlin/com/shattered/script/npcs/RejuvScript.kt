c95b5c841ff15bb2fbb63fcc1a40e4dbe4f9be44
Grizzly <grizzlyentmt@gmail.com>
2021-03-22 01:38:41 -0500

    API Update / AOE Healing Rejuv

--- scripts/src/main/kotlin/com/shattered/script/npcs/RejuvScript.kt
+++ scripts/src/main/kotlin/com/shattered/script/npcs/RejuvScript.kt
@@ -5,15 +5,26 @@ import com.shattered.script.types.NPCScript
 
 class RejuvScript : NPCScript() {
 
-    override fun on_update(api: NpcAPI?) {
-        super.on_update(api)
+    override fun fornpc(): String {
+        return "rejuv"
+    }
 
-        val p = api?.zone?.players
-        p?.forEach { n ->
-            if (n.zone.is_within_distance(api, 360)) {
-                n.combat.heal(api, 15)
+    override fun on_awake(api: NpcAPI) {
+        super.on_awake(api)
+        api.npc.lifeSpan = 5
+    }
+
+    override fun on_update(npc: NpcAPI) {
+        super.on_update(npc)
+        val players = npc.zone?.players
+
+        players?.forEach { p ->
+            if (npc.zone.is_within_distance(p, 360)) {
+                p.combat.heal(npc, 15)
+                println("We are healing player ${p.name}")
             }
         }
+
     }
 
 }
